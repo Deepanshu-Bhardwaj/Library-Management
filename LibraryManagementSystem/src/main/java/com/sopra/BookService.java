520c451f9c22a2b61b53d80dba6e6ce37ac3908e
Deepanshu Bhardwaj <dbhardwaj@axway.com>
2022-08-26 11:49:04 +0530

    initial comit

---
+++ LibraryManagementSystem/src/main/java/com/sopra/BookService.java
@@ -0,0 +1,17 @@
+package com.sopra;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sopra.repository.BookRepository;
+
+@Service
+public class BookService {
+	
+	@Autowired
+	BookRepository repo;
+	public Iterable<Book> findAll(){
+		return repo.findAll();
+	}
+
+}
